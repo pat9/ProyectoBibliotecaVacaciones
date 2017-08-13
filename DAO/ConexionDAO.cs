944eec6a25707af10977cf745d5e35a69ef069e5
Daniel Pat <danielpat6@gmail.com>
2017-08-12 22:09:03 -0500

    Agregar archivos de proyecto.

---
+++ DAO/ConexionDAO.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class ConexionDAO
+    {
+        string CadenaConexion = "";
+        SqlConnection Conexion;
+        SqlCommand Comando;
+        SqlDataAdapter Adap;
+        public ConexionDAO()
+        {
+            Comando = new SqlCommand();
+            Adap = new SqlDataAdapter();
+        }
+
+        public SqlConnection Conectar()
+        {
+            return Conexion = new SqlConnection(CadenaConexion);
+        }
+        
+        public void Abrir()
+        {
+            Conexion.Open();
+        }
+
+        public void Cerrar()
+        {
+            Conexion.Close();
+        }
+
+        public int EjecutarComando(SqlCommand comando)
+        {
+            Comando = comando;
+            Comando.Connection = Conectar();
+            Abrir();
+            int id = 0; int.TryParse(Comando.ExecuteScalar().ToString(), out id);
+            Cerrar();
+            return id;
+        }
+
+        public DataSet EjecutarSentencia(SqlCommand comando)
+        {
+            DataSet DS = new DataSet();
+            Comando = comando;
+            Comando.Connection = Conectar();
+            Adap.SelectCommand = Comando;
+            Abrir();
+            Adap.Fill(DS);
+            Cerrar();
+            return DS;
+        }
+
+        public DataSet EjecuatarSentencia(string comando)
+        {
+            DataSet DS = new DataSet();
+            Comando.CommandText = comando;
+            Comando.Connection = Conectar();
+            Adap.SelectCommand = Comando;
+            Abrir();
+            Adap.Fill(DS);
+            Cerrar();
+            return DS;
+        }
+
+    }
+}
